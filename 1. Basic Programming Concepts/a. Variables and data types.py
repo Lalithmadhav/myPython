6b41fc2d766259d48ad835dd85cfdecf33a83c48
Lalith Madhav <lalithmadhav7875@gmail.com>
2024-12-18 13:58:35 +0530

    Add program on Variables and Data Types

---
+++ 1. Basic Programming Concepts/a. Variables and data types.py
@@ -0,0 +1,25 @@
+# Variable: containers for storing data
+
+# # Data Type: String (to assign a text to a variable)
+# country = "India" # here country is variable and a string "India" is the value assigned to the variable
+# print(country, type(country))
+
+# # Data Type: Boolean (to evaluate expressions in Python)
+# # Boolean: True or False
+# happy = True 
+# print(happy, type(happy))
+# # Evaluate expressions
+# print(1<2)
+# print(1>2)
+
+# # Data Type: Integer (indicates integer numbers i.e. -∞ to ∞) (press Alt+2+3+6 for ∞)
+# age = 20
+# print(age, type(age))
+
+# # Data Type: Float (indicates decimal numbers)
+# height = 1.7 #meters
+# print(height, type(height))
+
+# Data Type: List ()
+myList = ["Mango", False, 8, 7.8, [10]]
+print(myList, type(myList))
